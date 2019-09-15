c110c7bacd16d234ff666fe7015267a7080daf1c
Miles Steele <miles@milessteele.com>
2019-09-14 20:45:53 -0400

    comment

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,3 @@
-//! Records a WAV file (roughly 3 seconds long) using the default input device and format.
-//!
-//! The input data is recorded to "$CARGO_MANIFEST_DIR/recorded.wav".
-
 extern crate cpal;
 extern crate failure;
 
